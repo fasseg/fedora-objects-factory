d6c98d1b33a24474875d13f69c025c929fd99a20
frank asseg <frank.asseg@fiz-karlsruhe.de>
2011-12-14 13:13:13 +0100

    fixed a bug in the questionary and added handling of long values

--- src/main/java/org/fcrepo/dto/factories/cli/Questionary.java
+++ src/main/java/org/fcrepo/dto/factories/cli/Questionary.java
@@ -15,46 +15,53 @@ public class Questionary {
 
 	@SuppressWarnings("unchecked")
 	protected <T extends Object> T poseQuestion(Class<T> resultClazz, T defaultValue, String question) throws Exception {
-		boolean valid=false;
-		T result=null;
-		while (!valid){
-			out.print(question);
-			String input = reader.readLine();
-			if (input.length() == 0) {
-				result=defaultValue;
-				valid=true;
-			}
-			if (resultClazz == String.class) {
-				result=(T) input;
-				valid=true;
-			} else if (resultClazz == Integer.class) {
-				try{
-					result=(T) new Integer(input);
-					valid=true;
-				}catch(NumberFormatException e){
-					printError(input);
+		boolean valid = false;
+		T result = null;
+		String input = null;
+		while (!valid) {
+			try {
+				out.print(question);
+				input = reader.readLine();
+				if (input.length() == 0) {
+					result = defaultValue;
+					valid = true;
+				}else{
+					if (resultClazz == String.class) {
+						result = (T) input;
+						valid = true;
+					} else if (resultClazz == Integer.class) {
+						result = (T) new Integer(input);
+						valid = true;
+					} else if (resultClazz == Long.class) {
+						result = (T) new Long(input);
+						valid=true;
+					} else if (resultClazz == Boolean.class) {
+						// Booleans need a little more handling than the rest
+						// Since "no", "yes" and the like should be checked too
+						String arg = input.toLowerCase();
+						if (arg.equals("no") || arg.equals("n") || arg.equals("false")) {
+							result = (T) Boolean.FALSE;
+							valid = true;
+						} else if (arg.equals("yes") || arg.equals("y") || arg.equals("true")) {
+							result = (T) Boolean.TRUE;
+							valid = true;
+						} else {
+							printError(input);
+						}
+					} else if (resultClazz == File.class) {
+						result = (T) new File(input);
+						valid = true;
+					} else {
+						throw new IllegalArgumentException("unable to handle results of type " + resultClazz.getName());
+					}
 				}
-			} else if (resultClazz == Boolean.class) {
-				String arg = input.toLowerCase();
-				if (arg.equals("no") || arg.equals("n") || arg.equals("false")) {
-					result= (T) Boolean.FALSE;
-					valid=true;
-				} else if (arg.equals("yes") || arg.equals("y") || arg.equals("true")) {
-					result= (T) Boolean.TRUE;
-					valid=true;
-				} else {
-					printError(input);
-				}
-			}else if(resultClazz == File.class){
-				result=(T) new File(input);
-				valid=true;
-			} else {
-				throw new IllegalArgumentException("unable to handle results of type " + resultClazz.getName());
+			} catch (NumberFormatException e) {
+				printError(input);
 			}
 		}
 		return result;
 	}
-
+	
 	private void printError(String input) throws Exception {
 		System.err.println("Unable to parse input '" + input + "'.Please try again");
 		Thread.sleep(500);
